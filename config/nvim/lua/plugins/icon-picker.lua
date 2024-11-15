3c91dcc98735e20a4d42770e3b989a7645c973e9
Charles Smith <charsmith@netflix.com>
2024-11-15 15:49:28 -0500

    recent changes

---
+++ config/nvim/lua/plugins/icon-picker.lua
@@ -0,0 +1,11 @@
+return {
+  "ziontee113/icon-picker.nvim",
+  config = function()
+    require("icon-picker").setup({ disable_legacy_commands = true })
+
+    local opts = { noremap = true, silent = true }
+
+    vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
+    vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
+  end,
+}
--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -1,66 +1,66 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-    },
-    config = function()
-      local lsp_zero = require("lsp-zero")
-      lsp_zero.extend_lspconfig()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+		},
+		config = function()
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_lspconfig()
 
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({ buffer = bufnr })
-      end)
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			lsp_zero.on_attach(function(client, bufnr)
+				-- see :help lsp-zero-keybindings
+				-- to learn the available actions
+				lsp_zero.default_keymaps({ buffer = bufnr })
+			end)
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "ruff", "biome" },
-        handlers = {
-          function(server_name)
-            require("lspconfig")[server_name].setup({})
-          end,
-          pyright = function()
-            require("lspconfig").pyright.setup({
-              settings = {
-                pyright = {
-                  -- Using Ruff's import organizer
-                  disableOrganizeImports = true,
-                },
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright", "ruff", "biome" },
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
+					pyright = function()
+						require("lspconfig").pyright.setup({
+							settings = {
+								pyright = {
+									-- Using Ruff's import organizer
+									disableOrganizeImports = true,
+								},
 
-                python = {
-                  analysis = {
-                    -- Ignore all files for analysis to exclusively use Ruff for linting
-                    ignore = { "*" },
-                  },
-                },
-              },
-            })
-          end,
-        },
-      })
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
+								python = {
+									analysis = {
+										-- Ignore all files for analysis to exclusively use Ruff for linting
+										ignore = { "*" },
+									},
+								},
+							},
+						})
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-        },
-      })
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+				},
+			})
 
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
